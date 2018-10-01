4ee6b64171f2e2886b34a844742b94d5ddddc392
Mikhail Amod <mickeyamod@gmail.com>
2018-10-01 13:55:07 +0200

    Fixed error

--- src/Pricing.java
+++ src/Pricing.java
@@ -9,21 +9,22 @@ public class Pricing {
         long n = scanner.nextLong();
         scanner.nextLine();
 
-        long x = findk(n, n/2);
+        long c = n/2;
+        while(c > 999999){
+            c = c/2;
+        }
+        long x = findk(n, c);
         System.out.println(x);
     }
 
     static long findk(long n, long count){
+        if(count < 2){ count = 2; }
         long sum = calculate(count);
         long sum2 = calculate(count+1);
         long minCount = 0;
-        int c = 0;
         while(true)
         {
-            c++;
-            System.out.println("count = " + count + ", sum = " + sum + ", sum2 = " + sum2);
-            if(c > 4){return -1L;}
-            if(sum < n && sum2 > n){
+            if(sum <= n && sum2 > n){
                 return count;
             }
             else if(sum > n && sum2 < n)
@@ -38,13 +39,9 @@ public class Pricing {
                 sum2 = calculate(count+1);
             }
             else if(sum > n){
-                long oldmin = minCount;
-                minCount = count;
-                count = count-(count-oldmin)/2;
+                count = count-(count-minCount)/2;
                 while(count > 999999){
-                    oldmin = minCount;
-                    minCount = count;
-                    count = count-(count-oldmin)/2;
+                    count = count-(count-minCount)/2;
                 }
                 sum = calculate(count);
                 sum2 = calculate(count+1);
