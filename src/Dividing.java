9107933e18d01e579716ad1052dc9b927beba940
Mikhail Amod <mickeyamod@gmail.com>
2018-10-02 08:33:16 +0200

    Code clean up

--- src/Dividing.java
+++ src/Dividing.java
@@ -10,10 +10,12 @@ public class Dividing {
         int n = scanner.nextInt();
         scanner.nextLine();
 
+        //get list of lengths from input
         int count = 0;
-        int maxLength = scanner.nextInt();
+        int maxLength = scanner.nextInt();//the biggest length we have
         plankLengths.add(maxLength);
         scanner.nextLine();
+        //get rest of inputs, updating the max
         while (count != n-1){
             int temp = scanner.nextInt();
             if(temp > maxLength){ maxLength= temp; }
@@ -24,33 +26,40 @@ public class Dividing {
         int min = scanner.nextInt();
         scanner.nextLine();
 
-        //System.out.println("N = " + n +
-        //        "\nmin = " + min);
-
         int m = findM(min, plankLengths, maxLength);
         System.out.println(m);
-        //System.out.println(plankLengths.toString());
 
     }
 
+    /**
+     * Find the maximum number of planks to fufill order, using divide and conquer
+     * This algorithm is O(n log n), I think :)
+     * @param minNumPlanks - min number of planks we need (K)
+     * @param listOfLengths - list of all the different lengths (Li)
+     * @param maxLength - the biggest length
+     * @return - the maximum number of planks (M)
+     */
     static int findM(int minNumPlanks, ArrayList<Integer> listOfLengths, int maxLength){
-        int plankLength = maxLength/2;
+        int plankLength = maxLength/2;//start at half the maximum length
         int oldLength = 0;
-        int max = 0;
+        int max = 0;//return the max
         while(true){
-            if(max == plankLength || plankLength == oldLength){
+            if(max == plankLength || plankLength == oldLength){//stopping condition
                 break;
             }
+            //calculate how many planks we can get if we divide each plank by the given length
             int numberOfPlanks = 0;
             for (int i = 0; i < listOfLengths.size(); i++) {
                 numberOfPlanks += listOfLengths.get(i)/plankLength;
             }
+
+            //current plankLength was good, try higher lengths
             if(numberOfPlanks >= minNumPlanks && plankLength > max){
                 max = plankLength;
                 oldLength = plankLength;
                 plankLength = plankLength + (maxLength-plankLength)/2;
             }
-            //not enough planks made, go down
+            //not enough planks made, go lower
             else{
                 oldLength = plankLength;
                 plankLength = plankLength - (plankLength-max)/2;
